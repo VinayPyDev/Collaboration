0ba15206258d0f2edf23afa801a96ea410a085b5
VinayPyDev <vinayxsupreme@gmail.com>
2026-09-16 17:49:52 +0530

    tilemap and minigame1 assets

---
+++ minigame1/minigame_tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="minigame_tileset1" tilewidth="32" tileheight="32" tilecount="7" columns="7">
+ <image source="C:/Users/Tanvi Technology/Documents/minigame_tileset1.png" width="224" height="32"/>
+</tileset>
