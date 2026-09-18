3a59c755c8a8212bd1d77782d3ba2de32b738d65
VinayPyDev <vinayxsupreme@gmail.com>
2026-09-18 17:36:54 +0530

    adding tileset 1

--- minigame1/minigame_tileset1.tsx
+++ minigame1/minigame_tileset1.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.2" name="minigame_tileset1" tilewidth="32" tileheight="32" tilecount="7" columns="7">
  <image source="C:/Users/Tanvi Technology/Documents/minigame_tileset1.png" width="224" height="32"/>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.10625" y="13.7727" width="15.1894" height="14.0876"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.546" y="5.11559" width="10.2312" height="21.2494"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="6">
+   <object id="10" x="0" y="0.236104" width="31.7954" height="31.4806"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="0.0787014" y="0.393507" width="31.8741" height="31.2445"/>
+  </objectgroup>
+ </tile>
 </tileset>
